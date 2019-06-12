8813fcc3e69715d939a028f2fd53ec7e52afdd50
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2019-06-12 23:11:41 +0200

    Finished game system and DRYed it by roughly 80%!

--- script.js
+++ script.js
@@ -1,39 +1,34 @@
 "use strict";
-// Konstruktor.
-function Scene(chapter, name, narration, choices) {
-  this.chapter = chapter;
-  this.name = name;
-  this.narration = narration;
-  this.choices = choices;
-};
-// Początkowa scena.
-let stage = new Scene(1, "Początek", "Znajdujesz się w ciemnym miejscu, czujesz że całe twoje ciało przeszywa tępy ból lecz przed sobą widzisz zupełną ciemnośc i pustkę.", [ "Próbuje się poruszyć", "Leże spokojnie"]);
-// Zmienne narracji i wyborów.
-const narr = document.getElementById("narration");
-const choi = document.getElementById("choices");
-// System podmiany obiektów i wypisywania ich w odpowiedni sposób.
-const sceneSwap = function sceneSwap(){
-  narr.innerHTML = stage.narration;
-  choi.innerHTML = "";
-  stage.choices.forEach(element => {
-    const choice = document.createElement('div');
-    choice.id = "choice-" + parseInt(stage.choices.indexOf(element) + 1);
-    choice.innerHTML = element;
-    choi.appendChild(choice);
-  });
-}
-sceneSwap();
-// Fabuła. PRACA
-choi.addEventListener("click", function(event) {
-  debugger;
-  switch (event.target.id) {
-    case "choice-1":
-      stage = new Scene(2, "Przebudzenie", "Obudziłeś się..", ["Ale mnie wszystko boli.."]);
-      sceneSwap();
-      break;
-    case "choice-2":
-      stage = new Scene(3, "Przebudzenie2", "Obudziłeś się..2", ["Ale mnie wszystko boli..2"]);
-      sceneSwap();
-      break;
+debugger;
+(function() {
+  // Konstruktor.
+  function Scene(narration, choices, references) {
+    this.narration = narration;
+    this.choices = choices;
+    this.references = references;
+  };
+  // System podmiany obiektów i wypisywania ich w odpowiedni sposób.
+  const sceneSwap = function sceneSwap(stage){
+    // Zmienne narracji i wyborów.
+    const narr = document.getElementById("narration");
+    const choi = document.getElementById("choices");
+    narr.innerHTML = stage.narration;
+    choi.innerHTML = "";
+    stage.choices.forEach(element => {
+      const choice = document.createElement('div');
+      choice.id = "choice-" + parseInt(stage.choices.indexOf(element) + 1);
+      choice.innerHTML = element;
+      choice.addEventListener("click", function() {
+        sceneSwap(stage.references[parseInt(stage.choices.indexOf(element))]);
+      });
+      choi.appendChild(choice);
+    });
   }
-}, false);
+  // Sceny, w kolejności ODWROTNEJ z powodu czytania JS.
+  // Mój syntax zapisywania nazw scen! (nazwaRozdzialu_numerSceny = ...)
+  const poczatek_3 = new Scene("W środku ciemności spędzasz dużo czasu, ale nie czujesz jego przeplywu. Zastanawiając się gdzie się znajdujesz powoli zauważasz że ból znika oraz odzyskujesz czucie w ciele", ["Postanawiam się poruszyć"], []);
+  const poczatek_2 = new Scene("W trakcie wielkiemu bólu i męczarni jaką ci to sprawia z dala, oślepia cię szybko pojawiający się z daleka blask. Widzisz pewne twarze nad sobą oraz slyszysz galop koni, sylwetki zwrócily na ciebie uwage i podeszy bliżej cibie", ["Spróbuj się poruszyć"], []);
+  const poczatek_1 = new Scene("Znajdujesz się w ciemnym miejscu, nie możesz się poruszyć, czujesz że całe twoje ciało przeszywa tępy ból lecz przed sobą widzisz zupełną ciemnośc i pustkę.", ["Próbuje się poruszyć", "Pozostań dalej w ciemności"], [poczatek_2, poczatek_3]);
+  // Rozpoczęcie gry.
+  sceneSwap(poczatek_1);
+}());
